aac6ebfc0fefc97d23c101b8cf62aeacda42c397
zhouyu <994212048@qq.com>
2018-11-02 09:57:20 +0800

    修改默认pid字段

--- src/Tree.php
+++ src/Tree.php
@@ -6,10 +6,10 @@ class Tree
     /**
      * 获取一个附加层级的一维数组
      */
-    static public function getArray ($cate, $html = '--', $pid = 0, $level = 0) {
+    static public function getArray ($cate,$pidname='pid', $html = '--', $pid = 0, $level = 0) {
         $arr = array();
         foreach ($cate as $k => $v) {
-            if ($v['pid'] == $pid) {
+            if ($v[$pidname] == $pid) {
                 $v['level'] = $level + 1;
                 $v['html']  = str_repeat($html, $level);
                 $arr[] = $v;
@@ -21,10 +21,10 @@ class Tree
     /**
      * 获取一个树形数组
      */
-    static public function getTree ($cate, $name = 'child', $pid = 0, $level = 0) {
+    static public function getTree ($cate,$pidname='pid', $name = 'child', $pid = 0, $level = 0) {
         $arr = array();
         foreach ($cate as $v) {
-            if ($v['pid'] == $pid) {
+            if ($v[$pidname] == $pid) {
                 $v['level'] = $level + 1;
                 $v[$name] = self::getTree($cate, $name, $v['id'], $level + 1);
                 $arr[] = $v;
@@ -33,21 +33,21 @@ class Tree
         return $arr;
     }
     //传递一个子分类ID返回所有的父级分类
-    static public function getParents ($cate, $id) {
+    static public function getParents ($cate, $id,$pidname='pid') {
         $arr = array();
         foreach ($cate as $v) {
             if ($v['id'] == $id) {
                 $arr[] = $v;
-                $arr = array_merge(self::getParents($cate, $v['pid']), $arr);
+                $arr = array_merge(self::getParents($cate, $v[$pidname]), $arr);
             }
         }
         return $arr;
     }
     //传递一个父级分类ID返回所有子分类ID
-    Static Public function getChildsId ($cate, $pid) {
+    Static Public function getChildsId ($cate, $pid,$pidname='pid') {
         $arr = array();
         foreach ($cate as $v) {
-            if ($v['pid'] == $pid) {
+            if ($v[$pidname] == $pid) {
                 $arr[] = $v['id'];
                 $arr = array_merge($arr, self::getChildsId($cate, $v['id']));
             }
@@ -58,7 +58,7 @@ class Tree
     static public function getChilds ($cate, $pid) {
         $arr = array();
         foreach ($cate as $v) {
-            if ($v['pid'] == $pid) {
+            if ($v[$pidname] == $pid) {
                 $arr[] = $v;
                 $arr = array_merge($arr, self::getChilds($cate, $v['id']));
             }
